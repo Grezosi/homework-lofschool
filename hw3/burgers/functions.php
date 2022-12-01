16cb68f31b62164315b9da43b59471d3745c86e0
Mikhail Kocharov <grezosi@gmail.com>
2022-12-01 18:59:01 +0300

    Added functions

--- hw3/burgers/functions.php
+++ hw3/burgers/functions.php
@@ -1,14 +1,48 @@
 <?php
 
 
-function connect(): object {
-    $pdo = new PDO('mysql:host=localhost; dbname=Burgers', 'root', null);
-    return $pdo;
+function connectDb() {
+    return new PDO('mysql:host=localhost; dbname=Burgers', 'root', null);
 }
 
 
-function select(string $query)
+
+
+function getUserId($email, PDO $pdo)
 {
-    connect();
-    $q = $pdo->prepare($query);
+    $q = $pdo->prepare("select `id` from `users` where email = ?");
+    $q->execute([$email]);
+    return $q->fetchColumn();
+}
+
+function insertUser($name, $email, $phone, PDO $pdo)
+{
+    $q = $pdo->prepare("insert into `users` (`name`, `phone`, `email`, `order_count`) values (?, ?, ?, 0)");
+    $q->execute([$name, $phone, $email]);
+}
+
+function orderCountUpd($email, PDO $pdo)
+{
+   $q = $pdo->prepare("update `users` set `order_count` = `order_count` + 1 where email = ?");
+   $q->execute([$email]);
+}
+
+function insertOrder($userId, $adress, PDO $pdo)
+{
+    $q = $pdo->prepare("insert into `orders` (`user_id`, `adress`) values (?, ?)");
+    $q->execute([$userId, $adress]);
+}
+
+function getOrderData($user_id, $order_id, PDO $pdo)
+{
+    $q = $pdo->prepare("select `adress`, `id` from `orders` where `user_id` = ? and `id` = ?");
+    $q->execute([$user_id, $order_id]);
+    return $q->fetchAll();
+}
+
+function getOrdersCount($email, PDO $pdo)
+{
+    $q = $pdo->prepare("select `order_count` from `users` where email = ?");
+    $q->execute([$email]);
+    return $q->fetchColumn();
 }
--- hw3/burgers/orders.php
+++ hw3/burgers/orders.php
@@ -1,36 +1,30 @@
 <?php
-
+require ('functions.php');
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 $adress =  'Улица ' . $_POST['street'] . '. Дом '. $_POST['home'] .  '. Подъезд '. $_POST['part'] . '. Квартира '. $_POST['appt'].  '. Этаж '. $_POST['floor'];
 $userId = '';
-//var_dump($name, $phone, $email);
 
-$pdo = new PDO('mysql:host=localhost; dbname=Burgers', 'root', null);
+$pdo = connectDb();
 
-    $query = $pdo->query("select * from `users` where email = '$email'");
-    $user = $query->fetchAll(PDO::FETCH_ASSOC);
+   $userId = getUserId($email, $pdo);
 
-    If (!$user) {
-         $query = $pdo->prepare("insert into `users` (`name`, `phone`, `email`, `order_count`) values (?, ?, ?, 1)");
-         $query->execute([$name, $phone, $email]);
-    } else {
-        $pdo->query("update `users` set `order_count` = `order_count` + 1 where email = '$email'");
+    If (is_bool($userId)) {
+        insertUser($name, $email, $phone, $pdo);
     }
-        $query = $pdo->query("select `id` from `users` where email = '$email'");
-        $userId = $query->fetchAll(PDO::FETCH_ASSOC)[0]['id'];
+    $userId = getUserId($email, $pdo);
 
- //   var_dump($adress);
- //   var_dump($id);
 
-   $pdo->query("insert into `orders` (`user_id`, `adress`) values ('$userId', '$adress')");
+    insertOrder($userId, $adress, $pdo);
 
-   $orderId = $pdo->lastInsertId();
-   $result = $pdo->query("select `adress`, `id` from `orders` where `user_id` = '$userId' and `id` = '$orderId'");
-   $res = $result->fetchAll(PDO::FETCH_ASSOC);
-   //var_dump($res);
+    $orderId = $pdo->lastInsertId();
+
+    orderCountUpd($email, $pdo);
+
+    $res = getOrderData($userId, $orderId, $pdo);
+
 
   foreach ($res as $k => $v) {
 
@@ -38,9 +32,6 @@ $pdo = new PDO('mysql:host=localhost; dbname=Burgers', 'root', null);
 "Номер вашего заказа: " . $v['id'] .  "<br>";
   }
 
-  $query = $pdo->query("select `order_count` from `users` where email = '$email'");
-  $res = $query->fetchAll(PDO::FETCH_ASSOC);
+  $orderCount = getOrdersCount($email, $pdo);
 
-foreach ($res as $v) {
-    echo "Это ваш " . $v['order_count']. "-й заказ!";
-  }
+  echo "Это ваш " . $orderCount. "-й заказ!";
